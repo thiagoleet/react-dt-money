d75ea89c31e2082e059467b15d6c6a0842f4dff1
Thiago Leite <thiagofmleite@gmail.com>
2025-01-20 17:11:48 -0300

    Integrate TransactionsContext into Summary component for transaction data access

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
+import TransactionsContext from "@/contexts/Transactions/context";
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
   return (
     <SummaryContainer>
       <SummaryCard>
