cc18868d81474b24d32b78f22b765c9fc9e731bd
Thiago Leite <thiagofmleite@gmail.com>
2025-01-20 17:11:39 -0300

    Wrap TransactionsPage with TransactionsProvider for transaction state management

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { TransactionsPage } from "./pages/Transactions";
+import TransactionsProvider from "./contexts/Transactions/provider";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <TransactionsPage />
+      <TransactionsProvider>
+        <TransactionsPage />
+      </TransactionsProvider>
     </ThemeProvider>
   );
 }
