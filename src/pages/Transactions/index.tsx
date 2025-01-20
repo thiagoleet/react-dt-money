dddae17fbde01befb5c0d26083cc690f1594b0a5
Thiago Leite <thiagofmleite@gmail.com>
2025-01-20 14:32:49 -0300

    Add TransactionsTable component with styled elements for transaction display

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,100 @@
 import { Summary } from "@/components/Summary";
 import { Header } from "../../components/Header";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+  TransactionsWrapper,
+} from "./styles";
 
 export function TransactionsPage() {
   return (
-    <main>
+    <TransactionsWrapper>
       <Header />
       <Summary />
-    </main>
+
+      <TransactionsContainer>
+        {" "}
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight $variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight $variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight $variant="outcome">
+                  - R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight $variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight $variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight $variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                -{" "}
+                <PriceHighlight $variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight $variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight $variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </TransactionsWrapper>
   );
 }
