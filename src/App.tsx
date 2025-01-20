8a362ed54578beaeaae2274eb3a58ab3c4a4bfcf
Thiago Leite <thiagofmleite@gmail.com>
2025-01-20 14:09:56 -0300

    Refactor App component to include GlobalStyle and TransactionsPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { TransactionsPage } from "./pages/Transactions";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>
-        <h1>App</h1>
-      </div>
+      <GlobalStyle />
+      <TransactionsPage />
     </ThemeProvider>
   );
 }
