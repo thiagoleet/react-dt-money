08802184faa72c6536745a66c43e001021929d1e
Thiago Leite <thiagofmleite@gmail.com>
2025-01-20 14:20:45 -0300

    Add Summary component with styled cards to display financial data

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
+import { Summary } from "@/components/Summary";
 import { Header } from "../../components/Header";
 
 export function TransactionsPage() {
   return (
     <main>
       <Header />
+      <Summary />
     </main>
   );
 }
